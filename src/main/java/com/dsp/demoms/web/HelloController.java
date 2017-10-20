21169b2fad3049b1b3ea13ecb803f5b5e012752f
admin <doushupeng403@berrygenomics.com>
2017-10-20 17:35:31 +0800

    swagger2 add ....

--- src/main/java/com/dsp/demoms/web/HelloController.java
+++ src/main/java/com/dsp/demoms/web/HelloController.java
@@ -2,14 +2,19 @@ package com.dsp.demoms.web;
 
 import com.dsp.demoms.dao.TimeSheetRepository;
 import com.dsp.demoms.entity.TimeSheet;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("测试使用")
 @PropertySource("classpath:application.properties")
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     @Autowired
@@ -21,7 +26,7 @@ public class HelloController {
     @Value("${info.key}")
     private String appKey;
 
-    @RequestMapping("/hello")
+    @RequestMapping("/index")
     public String index(){
         return "Hello Dou!\n"+ "Msg:" + appMsg + "\n" + "Key:" + appKey;
     }
@@ -30,7 +35,8 @@ public class HelloController {
      * 保存数据.
      * @return
      */
-    @RequestMapping("/save")
+    @ApiOperation(value="保存数据", notes="保存数据到时间表")
+    @RequestMapping(value = "/save", method = RequestMethod.GET)
     public String save(){
         // 内存数据库操作
        timeSheetRepository.save(new TimeSheet("title1", "content1"));
@@ -45,7 +51,8 @@ public class HelloController {
      * 获取所有数据.
      * @return
      */
-    @RequestMapping("/findAll")
+    @ApiOperation(value="查询数据", notes="查询时间表所有数据")
+    @RequestMapping(value = "/findAll", method = RequestMethod.GET)
     public Iterable<TimeSheet> findAll(){
         // 内存数据库操作
        return timeSheetRepository.findAll();
