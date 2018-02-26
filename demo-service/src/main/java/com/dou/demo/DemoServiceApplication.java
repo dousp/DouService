679f475d7739472b10df1b120dc8061014e24ea7
Dousp <douspeng@sina.cn>
2018-02-26 15:15:00 +0800

    eureka

--- demo-service/src/main/java/com/dou/demo/DemoServiceApplication.java
+++ demo-service/src/main/java/com/dou/demo/DemoServiceApplication.java
@@ -2,10 +2,12 @@ package com.dou.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableDiscoveryClient
 public class DemoServiceApplication {
 
 	public static void main(String[] args) {
